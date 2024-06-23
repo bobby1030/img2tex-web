89b0198c3302dc20b4f71eece85e27ea020e143e
Bobby Ho <angela.bobby@gmail.com>
2024-06-23 18:09:39 +0800

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
 	import '../app.css';
+	import Footer from './Footer.svelte';
 </script>
 
 <slot />
+
+<Footer />
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,17 @@
+<footer class="pb-8 text-sm text-center">
+	<p>
+		Based on image recognition model
+		<a href="https://huggingface.co/Norm/nougat-latex-base"
+			>Norm/nougat-latex-base</a
+		>, fine-tuned on <a href="https://huggingface.co/facebook/nougat-base">facebook/nougat-base</a>.
+	</p>
+	<p>Serverless inference provided by <a href="https://huggingface.co/docs/api-inference/">&#x1F917; Inference API</a>.</p>
+	<p>Hosted on Cloudflare Pages & Workers.</p>
+	<p>Source code on <a href="https://github.com/bobby1030/img2tex-web">GitHub</a>.</p>
+</footer>
+
+<style>
+    a {
+        @apply link link-info;
+    }
+</style>
