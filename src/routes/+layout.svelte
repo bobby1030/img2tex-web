8a8dcbddd8ecb72ac4eb5b305d3e3d2311e01275
Bobby Ho <angela.bobby@gmail.com>
2024-06-23 19:46:03 +0800

    metadata for SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script>
+	import { PUBLIC_BASE_URL } from '$env/static/public';
+
 	import '../app.css';
 	import Footer from './Footer.svelte';
 </script>
+
+<svelte:head>
+	<title>Img2TeX - Convert images with equations to TeX commands online.</title>
+	<meta name="description" content="Easily convert images with equations to TeX commands." />
+	<meta name="keywords" content="img2tex, image to tex, image to latex, image to math, image to equation" />
+	<meta name="og:image" content="{PUBLIC_BASE_URL}/og-image.png" />
+</svelte:head>
 
 <slot />
 
